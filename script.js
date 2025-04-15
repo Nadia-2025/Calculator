909aad0068505cf07bab9b7253096573553e9934
Nadia-2025 <nadyah-88@hotmail.com>
2025-04-15 11:24:05 +0200

    feat:Add JS functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+function appendNumber(number) {
+    document.getElementById("display").value += number;
+}
+
+function setOperation(operator) {
+    document.getElementById("display").value += operator;
+
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+
+    }
+    
+}
