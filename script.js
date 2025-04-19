a95f0a21a509dd1e2dbb9617a96e94c75e98c8f9
Nadia-2025 <nadyah-88@hotmail.com>
2025-04-19 17:17:13 +0200

    feat:Add localStorage

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+
 function appendNumber(number) {
     document.getElementById("display").value += number;
-}
+};
 
 function setOperation(operator) {
     document.getElementById("display").value += operator;
 
-}
+};
 
 function clearDisplay() {
     document.getElementById("display").value = "";
-}
+};
+
+let lastResult = localStorage.getItem("result");
+if (lastResult) { document.getElementById("display").value = lastResult;
+
+};
+    
 
 function calculate() {
     try {
         let result = eval(document.getElementById("display").value);
         document.getElementById("display").value = result;
 
+        localStorage.setItem("result", result);
+
     } catch (error) {
         document.getElementById("display").value = "Error";
 
     }
-    
-}
+};
